eb7a6e021d67574a6995506a85163572d838ff4a
Pavel Kuzmin <Virus191288@gmail.com>
2023-12-27 16:00:08 +0500

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <span>{{ timeLabel }}</span>
+</template>
+
+<script lang="ts" setup>
+defineI18nRoute({
+  locales: ['en']
+})
+
+const { $i18n } = useNuxtApp()
+
+await new Promise(resolve => setTimeout(resolve, 10))
+
+const timeLabel = computed(() => $i18n.t('general'))
+</script>
+
